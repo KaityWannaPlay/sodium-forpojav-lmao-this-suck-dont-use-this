af6894e52351e14e1d1c1aa45a23f8944b42c896
KaityWannaPlay <gamekaity@gmail.com>
2025-03-04 20:24:50 +0700

    Update PostLaunchChecks.java

--- common/src/workarounds/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
+++ common/src/workarounds/java/net/caffeinemc/mods/sodium/client/compatibility/checks/PostLaunchChecks.java
@@ -20,11 +20,12 @@ public class PostLaunchChecks {
         NvidiaWorkarounds.applyContextChanges(context);
 
         // FIXME: This can be determined earlier, but we can't access the GUI classes in pre-launch
-        if (isUsingPojavLauncher()) {
-            throw new RuntimeException("It appears that you are using PojavLauncher, which is not supported when " +
-                    "using Sodium. Please check your mods list.");
-        }
-    }
+        // if (isUsingPojavLauncher()) {
+            // throw new RuntimeException("It appears that you are using PojavLauncher, which is not supported when " +
+                    // "using Sodium. Please check your mods list.");
+        // }
+    // }
+        // i find it easy to just do this and trick pojav user so uhh ill just comment it here
 
     // https://github.com/CaffeineMC/sodium/issues/1916
     private static boolean isUsingPojavLauncher() {
